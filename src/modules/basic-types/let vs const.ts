2290e16d246f4ec3276f87d8cf3912a70e3b5d59
RenatoLPA <renatolucaspandrade@gmail.com>
2026-06-11 16:23:49 -0300

    let vs const

---
+++ src/modules/basic-types/let vs const.ts
@@ -0,0 +1,11 @@
+let phrase1 = 'Que a força esteja com você';
+
+//const phrase 2: 'Como deve ser'
+const phrase2 = 'Como deve ser';
+
+let phrase3 = 'Como tenho dito' as const;
+
+phrase3 = 'Como tenho dito';
+//phrase3 = 'xtz' Erro
+
+export default () => {};
